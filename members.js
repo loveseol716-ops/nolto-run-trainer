1f5a7ddce7f78f1fa1f432a0ef8b3dd5574f661f
loveseol716-ops <loveseol716@gmail.com>
2026-06-18 17:56:12 +0900

    Update members.js

--- members.js
+++ members.js
@@ -1,1 +1,16 @@
+const members = {
+  JH01: {
+    name: "설재현",
+    basePaceSeconds: 330
+  },
 
+  DW02: {
+    name: "윤다원",
+    basePaceSeconds: 360
+  },
+
+  TEST01: {
+    name: "테스트멤버",
+    basePaceSeconds: 330
+  }
+};
